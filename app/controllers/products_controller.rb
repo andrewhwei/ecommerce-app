2d40101e83a2276a3bee17b80cd173203f51d707
Andrew Wei <andrew.wei92@yahoo.com>
2016-04-14 20:05:11 -0700

    REST conventions added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  def products
+  def index
     @products = Product.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  get "/products", to: 'products#products'
+  get "/products", to: 'products#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
