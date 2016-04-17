d63904745456fa8c3e7f44548704c2afd37af968
Andrew Wei <andrew.wei92@yahoo.com>
2016-04-17 15:31:00 -0700

    added redirects

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,7 @@ class ProductsController < ApplicationController
   def create
     new_product = Product.new(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
     new_product.save
+    redirect_to "/products/#{new_product.id}"
   end
 
   def edit
@@ -24,6 +25,7 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find_by(id: params[:id])
     @product.update(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    redirect_to "/products/#{@product.id}"
     # Another method
     # @product.assign_attributes(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
     # @product.save
@@ -38,5 +40,6 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find_by(id: params[:id])
     @product.destroy
+    redirect_to "/products"
   end
 end
