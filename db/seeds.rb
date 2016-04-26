a9afa0b147da97d59ecdc43a936da5f594e65486
Andrew Wei <andrew.wei92@yahoo.com>
2016-04-25 19:12:59 -0700

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!([
+  {name: "Notebook", price: "1.0", image: "http://www.paperchase-usa.com/media/catalog/product/cache/4/image/956x956/9df78eab33525d08d6e5fb8d27136e95/00363720.JPG", description: "Paper notebook", in_stock: nil},
+  {name: "Star Wars OT", price: "80.0", image: "http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2015/11/Original-1.jpg", description: "VHS version of the Star Wars trilogy", in_stock: false},
+  {name: "Binder", price: "5.0", image: "http://www.staples-3p.com/s7/is/image/Staples/s0313728_sc7?$splssku$", description: "Three ring binder", in_stock: true},
+  {name: "Water bottle", price: "20.0", image: "https://robdogsblog.files.wordpress.com/2014/02/water-bottle-better.jpg", description: "fancy bottle", in_stock: true},
+  {name: "Tennis racket", price: "50.0", image: "http://www.mantis-sport.com/images/stories/shop/product/TSR350_racket25.png", description: "A racket for the sport of tennis", in_stock: true},
+  {name: "Pen", price: "1.12", image: "http://cdn.successories.com/products/81/815922/zoom_double_741220.jpg", description: "A normal ballpoint pen", in_stock: false},
+  {name: "Pencil", price: "8.0", image: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Colored-Pencils.jpg", description: "Set of 20 colored pencils", in_stock: true}
+])
