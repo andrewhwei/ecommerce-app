2c49ab1271830709257ad3d031859b025e83ad7e
Andrew Wei <andrew.wei92@yahoo.com>
2016-05-03 21:26:38 -0700

    Added user & admin authentication; as well as admin field in user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+
+
+  def authenticate_admin!
+    unless current_user && current_user.admin
+      redirect_to "/"
+    end
+  end
+
+  def authenticate_user!
+    unless current_user
+      redirect_to "/"
+    end
+  end
+
+
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_admin!, except: [:index, :show, :search]
 
   def index
     @categories = Category.all
@@ -27,7 +28,7 @@ class ProductsController < ApplicationController
   end
 
   def new
-    
+
   end
 
   def create
